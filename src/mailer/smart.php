0b75cedf02d980db5cefda6ec328adb9bfe469d6
IrinaSopizh <i.sopizhenko@gmail.com>
2021-06-14 17:39:24 +0300

    first commit

---
+++ src/mailer/smart.php
@@ -0,0 +1,43 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->Host = 'smtp.mailtrap.io';
+$mail->SMTPAuth = true;
+$mail->Port = 2525;
+$mail->Username = 'eb00f4a9392a42';
+$mail->Password = '96143cf4d928ea';                                   // TCP port to connect to
+ 
+$mail->setFrom('i.sopizhenko@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('i.sopizhenko@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
